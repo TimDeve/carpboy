9f7d536dc8d96e9ef3086fa3e1d3b2c45462e2d4
Tim Dévé <timdeve@users.noreply.github.com>
2020-04-20 22:53:29 +0100

    Adds buttons and sprites bindings

--- src/lib/arduboy.cpp
+++ src/lib/arduboy.cpp
@@ -1,7 +1,10 @@
 #include <Arduboy2.h>
+#include <Sprites.h>
+
 #include "arduboy.h"
 
 Arduboy2 arduboy;
+Sprites sprites;
 
 void Arduboy_begin()
 {
@@ -53,12 +56,97 @@ void Arduboy_fillRect(int16_t x, int16_t y, uint8_t w, uint8_t h, bool isWhite)
   arduboy.fillRect(x, y, w, h, isWhite ? WHITE : BLACK);
 }
 
+void Arduboy_initRandSeed()
+{
+  arduboy.initRandomSeed();
+}
+
 unsigned long Arduboy_genRandSeed()
 {
   return arduboy.generateRandomSeed();
 }
 
+void Arduboy_setFrameRate(int frameRate)
+{
+  arduboy.setFrameRate(frameRate);
+}
+
 void Arduboy_drawBitmap(int16_t x, int16_t y, const uint8_t *bmp, uint8_t w, uint8_t h)
 {
   arduboy.drawBitmap(x, y, bmp, w, h);
+}
+
+void Arduboy_pollButtons()
+{
+  arduboy.pollButtons();
+}
+
+bool Arduboy_upPressed()
+{
+  return arduboy.pressed(UP_BUTTON);
+}
+
+bool Arduboy_upJustPressed()
+{
+  return arduboy.justPressed(UP_BUTTON);
+}
+
+bool Arduboy_rightPressed()
+{
+  return arduboy.pressed(RIGHT_BUTTON);
+}
+
+bool Arduboy_rightJustPressed()
+{
+  return arduboy.justPressed(RIGHT_BUTTON);
+}
+
+bool Arduboy_downPressed()
+{
+  return arduboy.pressed(DOWN_BUTTON);
+}
+
+bool Arduboy_downJustPressed()
+{
+  return arduboy.justPressed(DOWN_BUTTON);
+}
+
+bool Arduboy_leftPressed()
+{
+  return arduboy.pressed(LEFT_BUTTON);
+}
+
+bool Arduboy_leftJustPressed()
+{
+  return arduboy.justPressed(LEFT_BUTTON);
+}
+
+bool Arduboy_aPressed()
+{
+  return arduboy.pressed(A_BUTTON);
+}
+
+bool Arduboy_aJustPressed()
+{
+  return arduboy.justPressed(A_BUTTON);
+}
+
+bool Arduboy_bPressed()
+{
+  return arduboy.pressed(B_BUTTON);
+}
+
+bool Arduboy_bJustPressed()
+{
+  return arduboy.justPressed(B_BUTTON);
+}
+
+void Arduboy_drawOverwrite(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t frame)
+{
+  sprites.drawOverwrite(x, y, bitmap, frame);
+}
+
+void Arduboy_drawSelfMasked(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t frame)
+{
+  sprites.drawSelfMasked(x, y, bitmap, frame);
 }
--- src/lib/arduboy.h
+++ src/lib/arduboy.h
@@ -16,6 +16,15 @@ extern "C"
   void Arduboy_drawBitmap(int16_t x, int16_t y, const uint8_t *bmp, uint8_t w, uint8_t h);
   void Arduboy_fillRect(int16_t x, int16_t y, uint8_t w, uint8_t h, bool isWhite);
   unsigned long Arduboy_genRandSeed();
+  void Arduboy_pollButtons();
+  bool Arduboy_upPressed();
+  bool Arduboy_rightPressed();
+  bool Arduboy_downPressed();
+  bool Arduboy_leftPressed();
+  bool Arduboy_aPressed();
+  bool Arduboy_bPressed();
+  void Arduboy_drawOverwrite(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t frame);
+  void Arduboy_drawSelfMasked(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t frame);
 
 #ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
 }
